66407a9984cf0876b2f03f067dbfeb8856365f8e
caprica-Six <dev@youpan.com>
2021-03-29 01:06:03 +0100

    Remove initials from Footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export default function Footer() {
   return (
     <footer role="contentinfo">
-      <StyledFooter>@2021 - Pepa Dom.</StyledFooter>
+      <StyledFooter>@2021.</StyledFooter>
     </footer>
   );
 }

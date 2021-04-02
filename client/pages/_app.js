f6cdd00b327bbd9a6d23939d2a3f6aae2dcc87fd
caprica-Six <dev@youpan.com>
2021-04-02 21:41:50 +0100

    Fix missing files on reLoad
    - move global styles to SComponents, app level
    - add babel plugin

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,48 @@
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
-import '../styles/globals.css';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html {
+      --white: #fff;
+      --red: #f52b2b;
+      --blue: rgb(31, 141, 231);
+      --black: #000;
+      --grey: #222;
+      --gray: var(--grey);
+      --lightGrey: #ececec;
+      --lightGray: var(--lightGrey);
+      --offWhite: #ededed;
+      --maxWidth: 1200px;
+      box-sizing: border-box;
+  }
+
+
+  *,
+  *:before,
+  *:after {
+      box-sizing: inherit;
+  }
+
+    body {
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      padding: 0;
+      margin: 0;
+      font-size: 1rem;
+      line-height: 1.5;
+      background-color: var(--lightGray);
+  }
+
+  a {
+      text-decoration: none;
+      color: var(--black);
+  }
+
+  a:hover {
+      text-decoration: underline;
+  }
+
+`;
 
 const client = new ApolloClient({
   // this is the server uri
@@ -7,10 +50,13 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-export default function MyApp({Component, pageProps}) {
+export default function App({Component, pageProps}) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <GlobalStyle />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 }

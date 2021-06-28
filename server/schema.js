43258cca3377edcbcde48becabc0a94f48ddfece
Alexandre <alexandre.danty@gmail.com>
2021-06-28 14:31:11 +0200

    back: new endpoint to get 2000 fake users.

--- server/schema.js
+++ server/schema.js
@@ -1,20 +1,52 @@
 import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-}  from "graphql";
-import faker  from "faker";
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+} from "graphql";
+import faker from "faker";
 
-export default new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "RootQueryType",
-    fields: {
-      name: {
-        type: GraphQLString,
-        resolve() {
-          return faker.name.findName();
+const myGraphQLSchema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "RootQueryType",
+        fields: {
+            name: {
+                type: GraphQLString,
+                resolve() {
+                    return faker.name.findName();
+                },
+            },
+            users: {
+                type: new GraphQLList(
+                    new GraphQLObjectType({
+                        name: "User",
+                        fields: {
+                            name: { type: GraphQLString },
+                            address: { type: GraphQLString },
+                            email: { type: GraphQLString },
+                            phone: { type: GraphQLString },
+                        },
+                    }),
+                ),
+                resolve() {
+                    return getUsers();
+                },
+            },
         },
-      },
-    },
-  }),
+    }),
 });
+
+function getUsers() {
+    const list = [];
+    for (let i = 0; i < 2000; i++) {
+        list.push({
+            name: faker.name.findName(),
+            address: faker.address.streetAddress(),
+            email: faker.internet.email(),
+            phone: faker.phone.phoneNumber(),
+        });
+    }
+    return list;
+}
+
+export default myGraphQLSchema;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { graphqlExpress, graphiqlExpress }  from "apollo-server-express";
-import myGraphQLSchema  from "./schema.js";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import myGraphQLSchema from "./schema.js";
 
 const app = express();
 
@@ -10,15 +10,15 @@ const app = express();
 app.use(cors());
 // bodyParser is needed just for POST.
 app.use(
-  "/graphql",
-  bodyParser.json(),
-  graphqlExpress({ schema: myGraphQLSchema })
+    "/graphql",
+    bodyParser.json(),
+    graphqlExpress({ schema: myGraphQLSchema }),
 );
 // for the graphiql interface
 app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`Graphql Server started on: http://localhost:${port}`);
+    if (err) throw err;
+    console.log(`Graphql Server started on: http://localhost:${port}`);
 });

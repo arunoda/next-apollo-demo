8e623e9d8cd664e8909c034ffe67683a2f4349cc
honohunter <bbbooo1994@gmail.com>
2021-06-30 19:28:15 +0100

    Improve security

--- client/next.config.js
+++ client/next.config.js
@@ -3,4 +3,17 @@ module.exports = {
     // Will be available on both server and client
     apiUrl: process.env.API_URL,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)?', // Matches all pages
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
 };
--- server/server.js
+++ server/server.js
@@ -2,18 +2,29 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const { ApolloServer, gql } = require("apollo-server-express");
 
 const schema = require("./schema");
 
 const port = process.env.PORT;
 
+console.log(process.env.NODE_ENV);
+
 async function startApolloServer() {
   const server = new ApolloServer({ schema });
   await server.start();
 
   const app = express();
-  app.use(cors());
+
+  if (process.env.NODE_ENV === "production") {
+    const corsOptions = {
+      origin: process.env.APP_URL,
+    };
+    app.use(helmet());
+    app.use(cors(corsOptions));
+  }
+
   server.applyMiddleware({ app });
 
   await new Promise((resolve) => app.listen({ port }, resolve));

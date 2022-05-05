8079e984bba748b0b503947c97ad8ae452e7d239
Venkat <venkat.n21@gmail.com>
2022-05-05 09:48:42 +0100

    Styling the Home Page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,12 +1,9 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default () => (
   <div>
     About Page
-    <br />
-    <br />
-    <Link href="/">
-      <a>Go Back</a>
-    </Link>
+    <br/><br/>
+    <Link href="/"><a>Go Back</a></Link>
   </div>
-);
+)

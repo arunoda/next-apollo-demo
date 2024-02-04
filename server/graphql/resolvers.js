6f0a8eddf2b86cfd5c22612d38dfa4d43a00f24b
Atif Mohammed <atif089@gmail.com>
2024-02-04 09:55:59 +0100

    added mock delay in resolver

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -3,6 +3,9 @@ import { employeeData } from "../data/index.js";
 export const resolvers = {
   Query: {
     employees: async (_, { skip = 0, limit = 20 }) => {
+      // added fake delay
+      await new Promise((resolve) => setTimeout(resolve, 100));
+
       // Return a paginated list of employees
       return employeeData.slice(skip, skip + limit);
     },

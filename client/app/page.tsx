d34471dcb14c3124e383d8c0e34f11bca006d2ab
Atif Mohammed <atif089@gmail.com>
2024-02-05 07:21:11 +0100

    reintegrate Apollo, the NextJS 13 way

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,34 @@
 import { Suspense } from "react";
+
+import { gql } from "@apollo/client";
+import { getClient } from "../lib/apollo-client";
+
 import EmployeesList from "../components/EmployeesList";
 import LoadingCard from "../components/LoadingCard";
 
 export default async function Page() {
+  const { data } = await getClient().query({
+    query: gql`
+      query GetEmployees($skip: Int!, $limit: Int!) {
+        employees(skip: $skip, limit: $limit) {
+          id
+          firstName
+          lastName
+          email
+          phoneNumber
+          address {
+            streetAddress
+            city
+          }
+        }
+      }
+    `,
+    variables: {
+      skip: 10,
+      limit: 10,
+    },
+  });
+
   return (
     <main>
       <Suspense
--- client/lib/apollo-client.ts
+++ client/lib/apollo-client.ts
@@ -1,9 +1,16 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-export const createApolloClient = () => {
-  const uri = process.env.API_URI || `http://localhost:5000/graphql`;
+export const { getClient } = registerApolloClient(() => {
+  const uri = process.env.NEXT_PUBLIC_API_URI;
   return new ApolloClient({
-    uri: uri,
     cache: new InMemoryCache(),
+    link: new HttpLink({
+      // this needs to be an absolute url, as relative urls cannot be used in SSR
+      uri,
+      // you can disable result caching here if you want to
+      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+      // fetchOptions: { cache: "no-store" },
+    }),
   });
-};
+});
